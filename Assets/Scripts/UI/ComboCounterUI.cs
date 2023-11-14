650c9478eb4a2e2a32f6f27792bba6c5def23929
Ismail Gok <ismailgok33@gmail.com>
2023-11-14 02:11:44 -0500

    ComboCounter script is added.

---
+++ Assets/Scripts/UI/ComboCounterUI.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ComboCounterUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI comboCounterText;
+    
+    private int _comboCounter = 0;
+    public float cooldown = 2f;
+    public float cooldownTimer;
+
+    private void Awake()
+    {
+        comboCounterText = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Start()
+    {
+        comboCounterText.text = "";
+        cooldownTimer = cooldown;
+    }
+
+    private void Update()
+    {
+        cooldownTimer -= Time.deltaTime;
+        if (cooldownTimer <= 0)
+        {
+            _comboCounter = 0;
+            comboCounterText.text = "";
+        }
+    }
+
+    private void UpdateComboCounter(object sender, EventArgs e)
+    {
+        _comboCounter++;
+        comboCounterText.text = _comboCounter.ToString();
+        
+        Animate();
+    }
+
+    private void Animate()
+    {
+        // TODO: Animate the combo counter when increased with a circular timer animation
+    }
+    
+    private void OnEnable()
+    {
+        Enemy.OnEnemyDeath += UpdateComboCounter;
+    }
+
+    private void OnDisable()
+    {
+        Enemy.OnEnemyDeath -= UpdateComboCounter;
+    }
+}
